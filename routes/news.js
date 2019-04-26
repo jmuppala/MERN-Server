325a878705424c62c7cc2cae4c80463203037159
Jogesh Muppala <jmuppala@hotmail.com>
2019-04-26 21:22:25 +0800

    REST API End

--- routes/news.js
+++ routes/news.js
@@ -24,4 +24,46 @@ router.route('/')
     next(err);
 });
 
+/* /news/:newsId REST API endpoint */
+router.route('/:newsId')
+.get((req, res, next) => {
+  let item = newsitems.find((item) => item._id === req.params.newsId);
+  if (item)
+    res.json(item);
+  else {
+    var err = new Error('Did not find item with index: ' + req.params.newsId + '!');
+    err.status = 404;
+    next(err);
+  }
+})
+.post((req, res, next) => {
+  var err = new Error('POST is forbidden on /news/:newsId!');
+  err.status = 403;
+  next(err);
+})
+.put((req, res, next) => {
+  let index = newsitems.findIndex((item) => item._id === req.params.newsId);
+  if (index < 0) {
+    var err = new Error('Did not find item with index: ' + req.params.newsId + '!');
+    err.status = 404;
+    next(err);
+  }
+  else {
+    newsitems.splice(index,1,req.body);
+    res.json(newsitems);
+  }
+})
+.delete((req, res, next) => {
+  let index = newsitems.findIndex((item) => item._id === req.params.newsId);
+  if (index < 0) {
+    var err = new Error('Did not find item with index: ' + req.params.newsId + '!');
+    err.status = 404;
+    next(err);
+  }
+  else {
+    newsitems.splice(index,1);
+    res.json(newsitems);
+  }
+});
+
 module.exports = router;
